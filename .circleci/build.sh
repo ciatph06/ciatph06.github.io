f919221048174614379987bfb108b1f54f4cf5c8
ciatph06 <ciatph06@gmail.com>
2019-09-05 05:26:15 +0800

    Added circle ci.

---
+++ .circleci/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -ev
+
+npm run build
+
+git config --global user.name ${GITHUB_USER}
+git config --global user.email ${GITHUB_EMAIL}
+
+git clone --quiet --branch=$BRANCH https://${GITHUB_API_TOKEN}@github.com/$TARGET_REPO website > /dev/null
+cd website
+git rm -rf .
+git add .
+cd ..
+
+cp -a dist/. website/.
+cd website
+git add -f .
+
+if git commit -m "Deploying new files..." ; then
+  git push -fq origin $BRANCH > /dev/null
+  echo -e "Deploy completed.\n"
+else
+  echo -e "Content not changed, nothing to deploy.\n"
+fi
